1b00e8ac64655bd645ed82c598a40b59f0214928
linguyen <nguyenvanlin01041994@gmail.com>
2016-09-20 08:31:59 +0700

    Finish user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'signup' => 'users#new'
+  #
+  resources:users
 end
